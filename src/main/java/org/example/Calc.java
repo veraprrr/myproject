3ad1cb5bec17c170ee1bbe34638b577529bfd3b2
veraprrr <veravera404@yandex.ru>
2025-10-05 10:52:34 +0500

    Initial commit

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calc {
+    public int summ(int a, int b){
+        int result;
+        result = a + b;
+        System.out.println("Сумма " + a + " и " + b + " равна " + result);
+        System.out.printf("Сумма %d и %d равна %d", a, b, result);
+        return result;
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calc calculator = new Calc();
+        calculator.summ(1, 10);
+    }
+}
