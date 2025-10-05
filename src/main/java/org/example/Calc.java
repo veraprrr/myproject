0bf0bec4b429df968c91679fbc92603f41a79aab
veraprrr <veravera404@yandex.ru>
2025-10-05 13:42:42 +0500

    testSuite added

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -4,8 +4,8 @@ public class Calc {
     public int summ(int a, int b){
         int result;
         result = a + b;
-        System.out.println("Сумма " + a + " и " + b + " равна " + result);
-        System.out.printf("Сумма %d и %d равна %d", a, b, result);
+        //System.out.println("Сумма " + a + " и " + b + " равна " + result);
+        System.out.printf("Сумма %d и %d равна %d\n", a, b, result);
         return result;
     }
 }
---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @BeforeAll
+    public static void beforeAll(){
+        System.out.println("before all");
+    }
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("before each");
+    }
+
+    @AfterAll
+    public static void afterAll(){
+        System.out.println("after all");
+    }
+
+    @AfterEach
+    public void afterEach(){
+        System.out.println("after each");
+    }
+
+    @Test
+    @DisplayName("Проверка суммирования 1")
+    @Timeout(10)
+    void summ1() {
+        Calc calc = new Calc();
+        int result = calc.summ(1, 6);
+        Assertions.assertEquals(7, result, "Не тот ответ");
+    }
+
+    @Test
+    @DisplayName("Проверка суммирования 2")
+    @Timeout(10)
+    void summ2() {
+        Calc calc = new Calc();
+        int result = calc.summ(1, 6);
+        Assertions.assertEquals(7, result, "Не тот ответ");
+    }
+
+    @ParameterizedTest (name = "#{index} - сложение {0} и {1}, ожидаем {2}")
+    @DisplayName("Прверка суммирования 3")
+    @CsvSource({"1, 2, 3", "-1, 2, 1", "0, 0, 0"})
+    @Tag("param")
+    void summ3(int a, int b, int expectedResult) {
+        Calc calc = new Calc();
+        int result = calc.summ(a, b);
+        Assertions.assertEquals(expectedResult, result, "Не тот ответ");
+    }
+}
